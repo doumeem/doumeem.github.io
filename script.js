e54336d97b77d9dca6c0d4a04b7c579eb62aa51e
Dou Meem <79662326+doumeem@users.noreply.github.com>
2025-02-25 08:06:28 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
-const $el = document.querySelector(".follower");
-window.addEventListener("mousemove", (e) => {
-    const { target, x, y } = e;
-    gsap.to($el, {
-        x: x-20,
-        y: y-15,
-        duration: 0.5,
-        opacity: 0.5,
-    });
-});
-window.addEventListener("mouseout", (e) => {
-    gsap.to($el, {
-        duration: 0.5,
-        opacity: 0,
-    });
-});
-
 const typed = new Typed(".typing", {
     strings: ["Developer", "Weeb", "Gamer", "Metalhead", "Streamer"],
     loop: true,
     typeSpeed: 100,
     backSpeed: 50,
-    cursorChar: ' |',
+    cursorChar: "",
 });
 
 $(".age").html(Math.abs(new Date(Date.now()-new Date("12/02/2007").getTime()).getUTCFullYear()-1970));
-
-setInterval(() => {
-    $.ajax({
-        url: "https://decapi.me/twitch/uptime/doumeem",
-        success: (data) => {
-            if (!data.includes("offline")) $(".twitch").addClass("live");
-            else $(".twitch").removeClass("live");
-        }
-    });
-}, 10000);
