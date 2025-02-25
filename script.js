edaf7a9988c8ef7ae5f502d66091223565b06efe
Dou Meem <79662326+doumeem@users.noreply.github.com>
2025-02-25 11:24:57 +0330

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const typed = new Typed(".typing", {
-    strings: ["Developer", "Weeb", "Gamer", "Metalhead", "Streamer"],
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    cursorChar: "",
-});
-
-$(".age").html(Math.abs(new Date(Date.now()-new Date("12/02/2007").getTime()).getUTCFullYear()-1970));
